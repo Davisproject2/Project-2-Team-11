2199dfa417c0002ca906ce3c4b286d7662de73c6
Davisproject2 <80134652+Davisproject2@users.noreply.github.com>
2021-03-11 20:34:14 -0800

    Update Project2code.R

--- Code/Project2code.R
+++ Code/Project2code.R
@@ -8,9 +8,9 @@ library(gganimate)
 
 # SET UP
 
-covid <- read_csv("https://covid19.who.int/WHO-COVID-19-global-data.csv")
-c_h_index <- read_csv("covid-containment-and-health-index.csv")
-pop_index <- read_csv("population.csv")
+covid <- read_csv("../Data/https://covid19.who.int/WHO-COVID-19-global-data.csv")
+c_h_index <- read_csv("../Data/covid-containment-and-health-index.csv")
+pop_index <- read_csv("../Data/population.csv")
 
 colnames(covid)[which(names(covid) == "Date_reported")] <- "Date"
 colnames(covid)[which(names(covid) == "Country_code")] <- "Code"
@@ -26,12 +26,11 @@ pop <- pop_index[c("Country Name","2019")]
 pop <- pop[!is.na(pop["2019"]),]
 
 covid_index_pop = merge(covid_index, pop, by.x = c("Country"), by.y = c("Country Name"))
-
 colnames(covid_index_pop)[which(names(covid_index_pop) == "2019")] <- "Population"
-
 covid_index_pop$new_cases_div_by_pop <- covid_index_pop$New_cases / covid_index_pop$Population
 
 # total cases worldwide to date
+
 total.cc = sum(covid$New_cases)
 a <- covid %>% group_by(Country) %>% 
   summarise(sum_newcase = sum(New_cases), max_newcase_perday_bycountry = max(New_cases)) %>% 
@@ -63,12 +62,11 @@ plot2 <- ggplot(data=covid, mapping = aes(x=Date, y=New_deaths)) +
 
 plot1 / plot2 + plot_layout(ncol = 1, heights=c(4,4))
 
-
 Italy <- covid[covid$Country == "Italy",]
 
 plot_Italy_c <- Italy %>% ggplot(aes(x=Date, y=New_cases)) + 
   geom_line(color = "darkorange", size = 0.5)+
-  labs(title = "Total Covid New Cases in Italy (Daily)",
+   labs(title = "Total Covid New Cases in Italy (Daily)",
        subtitle = "How covid cases increase in the Italy?",
        x = "Date Reported (Daily)",
        y = "# of New Cases") +
@@ -77,13 +75,13 @@ plot_Italy_c <- Italy %>% ggplot(aes(x=Date, y=New_cases)) +
 
 plot_Italy_d <- Italy %>% ggplot(aes(x=Date, y=New_deaths)) + 
   geom_line(color = "firebrick4", size = 0.5)+
-  labs(title = "Total New Covid Deaths in Italy (Daily)",
+   labs(title = "Total New Covid Deaths in Italy (Daily)",
        subtitle = "How covid deaths increase in the Italy?",
        x = "Date Reported (Daily)",
-       y = "# of New Deaths") +
+       y = "# of New Deaths",
+       color = "Country") +
   theme_fivethirtyeight() + 
   theme(axis.title = element_text()) 
-
 
 plot_Italy_c / plot_Italy_d + plot_layout(ncol = 1, heights=c(4,4))
 
@@ -91,7 +89,7 @@ Italy2 <- covid_index[covid_index$Country == "Italy",]
 
 plot_Italy_ci <- Italy2 %>% ggplot(aes(x=Date, y=containment_index)) + 
   geom_line(color = "lightpink4", size = 0.5)+
-  labs(title = "Containment Index score in Italy (Daily)",
+   labs(title = "Containment Index score in Italy (Daily)",
        subtitle = "How the containment inbdex changes in the Italy?",
        x = "Date Reported (Daily)",
        y = "containment index (0-100)") +
@@ -100,52 +98,29 @@ plot_Italy_ci <- Italy2 %>% ggplot(aes(x=Date, y=containment_index)) +
 
 plot_Italy_d <- Italy %>% ggplot(aes(x=Date, y=New_deaths)) + 
   geom_line(color = "firebrick4", size = 0.5)+
-  labs(title = "Total New Covid Deaths in Italy (Daily)",
+   labs(title = "Total New Covid Deaths in Italy (Daily)",
        subtitle = "How covid deaths increase in the Italy?",
        x = "Date Reported (Daily)",
        y = "# of New Deaths") +
   theme_fivethirtyeight() + 
-  theme(axis.title = element_text()) 
-
+  theme(axis.title = element_text())
 
 plot_Italy_d / plot_Italy_ci + plot_layout(ncol = 1, heights=c(4,4))
 
-US <- covid[covid$Country == "United States of America",] 
+US <- covid[covid$Country == c("United States of America","Brazil","India"),] 
 
-Brazil <- covid[covid$Country == "Brazil",]
-
-India <- covid[covid$Country == "India",]
-
-plot_US <- US %>% ggplot(aes(x=Date, y=New_cases)) + 
-  geom_line(color = "mediumblue",size = 0.5)+
-  labs(title = "Total Covid New Cases in US (Daily)",
-       subtitle = "How covid increases in the US?",
+plot_top3 <- US %>% ggplot(aes(x=Date, y=New_cases,color = Country)) + 
+  geom_line(size = 0.5)+
+   labs(title = "Total Covid New Cases in Top Three Countries (Daily)",
+       subtitle = "How covid increases in Top Three Countries?",
        x = "Date Reported (Daily)",
-       y = "# of New Cases") +
+       y = "# of New Cases") + 
+  theme(axis.title = element_text()) + 
   theme_fivethirtyeight() + 
-  theme(axis.title = element_text()) 
-
-plot_Brazil <- Brazil %>% ggplot(aes(x=Date, y=New_cases)) + 
-  geom_line(color = "saddlebrown", size = 0.5)+
-  labs(title = "Total Covid New Cases in Brazil (Daily)",
-       subtitle = "How covid increases in the Brazil?",
-       x = "Date Reported (Daily)",
-       y = "# of New Cases") +
-  theme_fivethirtyeight() + 
-  theme(axis.title = element_text()) 
-
-plot_India <- India %>% ggplot(aes(x=Date, y=New_cases)) + 
-  geom_line(color = "seagreen4", size = 0.5)+
-  labs(title = "Total Covid New Cases in India (Daily)",
-       subtitle = "How covid increases in the India?",
-       x = "Date Reported (Daily)",
-       y = "# of New Cases") +
-  theme_fivethirtyeight() + 
-  theme(axis.title = element_text())
-
-plot_US 
-plot_Brazil
-plot_India
+  theme(axis.title = element_text()) + 
+  theme(plot.title = element_text(size=16))
+  
+plot_top3
 
 covid$Date_of_Restrictions <- covid$Date - 28
 
@@ -161,7 +136,7 @@ colnames(covid_index_pop)[which(names(covid_index_pop) == "2019")] <- "Populatio
 covid_index_pop$new_cases_div_by_pop <- covid_index_pop$New_cases / covid_index_pop$Population
 
 plot_01_03_2020 <- covid_index_pop %>% ggplot() + 
-  geom_histogram(mapping = aes(x = containment_index), fill = "seagreen4", size = 0.5, binwidth = 1)+
+  geom_histogram(mapping = aes(x = containment_index), fill = "slategray3", size = 0.5, binwidth = 1)+
   labs(title = "Containment Index Distribution starting on 01/03/2020",
        x = "Containment Index",
        y = "Frequency") +
@@ -174,7 +149,7 @@ covid_post_0130 <- with(covid_index_pop, covid_index_pop[Date > "2020-01-29",])
 covid_post_0130
 
 plot_01_30_2020 <- covid_post_0130 %>% ggplot() + 
-  geom_histogram(mapping = aes(x = containment_index), fill = "seagreen4", size = 0.5, binwidth = 1)+
+  geom_histogram(mapping = aes(x = containment_index), fill = "slategray3", size = 0.5, binwidth = 1)+
   labs(title = "Containment Index Distribution starting on 01/30/2020",
        x = "Containment Index",
        y = "Frequency") +
@@ -187,7 +162,7 @@ covid_post_0311 <- with(covid_index_pop, covid_index_pop[Date > "2020-03-10",])
 covid_post_0311
 
 plot_03_11_2020 <- covid_post_0311 %>% ggplot() + 
-  geom_histogram(mapping = aes(x = containment_index), fill = "seagreen4", size = 0.5, binwidth = 1)+
+  geom_histogram(mapping = aes(x = containment_index), fill = "slategray3", size = 0.5, binwidth = 1)+
   labs(title = "Containment Index Distribution starting on 03/11/2020",
        x = "Containment Index",
        y = "Frequency") +
@@ -200,7 +175,7 @@ covid_post_01300310 <- with(covid_index_pop, covid_index_pop[Date > "2020-01-29"
 covid_post_01300310
 
 plot_01_30_thru_03_11_2020 <- covid_post_01300310 %>% ggplot() + 
-  geom_histogram(mapping = aes(x = containment_index), fill = "seagreen4", size = 0.5, binwidth = 1)+
+  geom_histogram(mapping = aes(x = containment_index), fill = "slategray3", size = 0.5, binwidth = 1)+
   labs(title = "Containment Index Distribution 01/30/2020 thru 03/11/2020",
        x = "Containment Index",
        y = "Frequency") +
